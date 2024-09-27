01c8c0110ee62e1fbf5861346c8840e7133ebfa0
Mustafa Abukhadra <abukhadra@users.noreply.github.com>
2024-09-27 10:34:55 +0300

    Delete node_modules directory

--- node_modules/seen-compiler/src/symtab.js
+++
@@ -1,4 +0,0 @@
-const funcs = []            // FIXME: workaround, storing fn names here , until name resolution  is complete.
-const structs = []          // FIXME: workaround , just like funcs
-
-export { funcs, structs }
